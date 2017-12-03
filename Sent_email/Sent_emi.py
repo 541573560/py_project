862de727d067f8c078b43d62c16772b87908a290
lihang <541573560@qq.com>
2017-12-03 21:10:11 +0800

    make the item more fluent

--- Sent_email/Sent_emi.py
+++ Sent_email/Sent_emi.py
@@ -5,18 +5,16 @@ from find_ChangchunToKunming.Spider_tiket import load_massage
 
 url = 'https://sjipiao.fliggy.com/flight_search_result.htm?_input_charset=utf-8&spm=181.7091613.a1z67.1001&searchBy=1280&tripType=0&depCityName=%E9%95%BF%E6%98%A5&depCity=&depDate=2018-01-13&arrCityName=%E6%98%86%E6%98%8E&arrCity=KMG&arrDate='
 
-
-_user = "541573560@qq.com"
-_pwd  = "rlapjidinhdnbegc"
-_to_list   = ["541573560@qq.com"]
-
-qqq = load_massage(url)
-
-msg = MIMEText(qqq)
-msg["Subject"] = u"机票信息"
-msg["From"]    = _user
-
 def sent_email():
+    _user = "541573560@qq.com"
+    _pwd = "rlapjidinhdnbegc"#u邮箱stmp服务的密码
+    _to_list = ["541573560@qq.com"]
+
+    qqq = load_massage(url)
+
+    msg = MIMEText(qqq)
+    msg["Subject"] = u"机票信息"
+    msg["From"] = _user
     try:
         s = smtplib.SMTP_SSL("smtp.qq.com", 465)
         s.login(_user, _pwd)
@@ -30,3 +28,4 @@ def sent_email():
 
 if __name__ =='__main__':
     sent_email()
+
